fa1df4762fdbe13df9e0065acacd7b06126387b7
shubhendu-the-artist <shubhendu.garai99.9@gmail.com>
2025-12-27 21:44:26 +0530

    Creating HomePage

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+function navButton() {
+  document.querySelectorAll("#nav a").forEach((link) => {
+    //     <span id="line1" class="line"></span>
+    // <span id="line2" class="line"></span>
+    const line1 = document.createElement("span");
+    const line2 = document.createElement("span");
+    line1.id = "line1";
+    line2.id = "line2";
+    line1.classList.add("line");
+    line2.classList.add("line");
+    link.appendChild(line1);
+    link.appendChild(line2);
+  });
+}
+navButton();
+
 function revealToSpan() {
   document.querySelectorAll(".reveal").forEach((elem) => {
     let parent = document.createElement("span");
@@ -14,12 +30,103 @@ function revealToSpan() {
   });
 }
 
-revealToSpan();
+function valueSet() {
+  gsap.set("#nav a", {
+    y: "-100%",
+    opacity: 0,
+  });
+  gsap.set("#home span .child", {
+    y: "100%",
+  });
+  gsap.set("#home .row img", {
+    opacity: 0,
+  });
 
-const tl = gsap.timeline();
-gsap.to(".parent .child", {
-  y: "-100%",
-  duration: 1,
-  delay: 2,
-  ease: Circ.EaseInOut
-});
+  document.querySelectorAll("#Web>g").forEach((e) => {
+    const character = e.childNodes[1].childNodes[1];
+
+    character.style.strokeDasharray = character.getTotalLength() + "vw";
+    character.style.strokeDashoffset = character.getTotalLength() + "vw";
+  });
+}
+
+function loaderAnimation() {
+  const tl = gsap.timeline();
+  tl.from("#loader .child span", {
+    x: 100,
+
+    duration: 1,
+    stagger: 0.2,
+    ease: Power3.easeInOut,
+  })
+    .to("#loader .parent .child", {
+      y: "-100%",
+      duration: 1,
+      ease: Circ.easeInOut,
+    })
+    .to("#loader", {
+      height: 0,
+      duration: 1,
+      ease: Circ.easeInOut,
+    })
+    .to("#green", {
+      height: "100%",
+      top: 0,
+      duration: 1,
+      delay: -0.8,
+      ease: Circ.easeInOut,
+    })
+    .to("#green", {
+      height: 0,
+      duration: 1.5,
+      delay: -0.3,
+      ease: Circ.easeInOut,
+      onComplete: function () {
+        animateHomePage();
+      },
+    });
+}
+
+function animateSVG() {
+  tl = gsap.timeline();
+  tl.from("#Web>g>g>path, #Visual>g>g>polyline", {
+    fill: "none",
+  })
+  .to("#Web>g>g>path, #Visual>g>g>polyline", {
+    strokeDashoffset: 0,
+    fill: "#14cf93",
+    duration: 2,
+    ease: Expo.easeInOut,
+  });
+}
+
+function animateHomePage() {
+  const tl = gsap.timeline();
+  tl.to("#nav a", {
+    y: 0,
+    opacity: 1,
+    stagger: 0.2,
+    ease: Expo.easeInOut,
+  })
+    .to("#home .parent .child", {
+      y: 0,
+      stagger: 0.1,
+      duration: 1.5,
+      delay: -1,
+      ease: Expo.easeInOut,
+      onComplete: function () {
+        animateSVG();
+      },
+    })
+    .to("#home .row img", {
+      opacity: 1,
+      duration: 2,
+      delay: 1.5,
+      y: "50%",
+      ease: "bounce.out",
+    });
+}
+
+revealToSpan();
+valueSet();
+loaderAnimation();
